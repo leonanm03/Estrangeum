2951fa96f97c2757f2938e2509391751de764c5c
Leonan Martins <leonanm04@gmail.com>
2023-06-19 20:02:08 -0300

    refactor: sign-up schema

--- backend-estrangeum/src/schemas/user-schemas.ts
+++ backend-estrangeum/src/schemas/user-schemas.ts
@@ -5,5 +5,5 @@ export const createUserSchema = Joi.object<CreateUserParams>({
   name: Joi.string().min(2).required(),
   email: Joi.string().email().required(),
   password: Joi.string().min(6).required(),
-  image_url: Joi.string().uri().optional(),
+  image_url: Joi.string().uri().allow(""),
 });
