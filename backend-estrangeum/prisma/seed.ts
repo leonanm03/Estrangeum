dc5b8b8f50a5079a7ac1ef555f23d5a1ecb7b653
Leonan Martins <leonanm04@gmail.com>
2023-06-23 16:19:38 -0300

    feat: objects seed

--- backend-estrangeum/prisma/seed.ts
+++ backend-estrangeum/prisma/seed.ts
@@ -1,16 +1,12 @@
 import { PrismaClient } from "@prisma/client";
+import objectSeed from "./objectSeed";
 
 const prisma = new PrismaClient();
 
-async function main() {}
-
-// imagens bule e xicara
-const images = [
-  "https://images.tcdn.com.br/img/img_prod/836126/jogo_2_pecas_porcelana_a_bela_e_a_fera_samovar_e_zip_bule_e_xicara_165_2_31f9cdd1394c2c720c55fe1c15b3772d.jpeg",
-  "https://images.tcdn.com.br/img/img_prod/836126/jogo_2_pecas_porcelana_a_bela_e_a_fera_samovar_e_zip_bule_e_xicara_165_3_892bec598a8bada32bfa6d30514869b7.jpeg",
-  "https://images.tcdn.com.br/img/img_prod/836126/jogo_2_pecas_porcelana_a_bela_e_a_fera_samovar_e_zip_bule_e_xicara_165_4_cea59fd3fef02b521cbe83bf17ee4b6c.jpeg",
-  "https://images.tcdn.com.br/img/img_prod/836126/jogo_bela_e_a_fera_madame_samovar_e_zip_2_pecas_165_1_20201125152303.jpeg",
-];
+async function main() {
+  const objects = await objectSeed();
+  console.log(objects);
+}
 
 main()
   .catch((e) => {
