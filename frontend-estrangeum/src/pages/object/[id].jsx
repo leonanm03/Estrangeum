27d9616ec377927eaa6bdc580db133a7ae4f5214
Leonan Martins <leonanm04@gmail.com>
2023-06-26 15:45:11 -0300

    feat: get object by id

---
+++ frontend-estrangeum/src/pages/object/[id].jsx
@@ -0,0 +1,28 @@
+import useObject from "@/hooks/api/useObject";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+
+export default function ObjectPage() {
+  const router = useRouter();
+  const { id } = router.query;
+  console.log(id);
+  const { getObject } = useObject();
+  const [objectData, setObjectData] = useState(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      if (id !== undefined) {
+        try {
+          const object = await getObject(id);
+          console.log(object);
+          setObjectData(object);
+        } catch (error) {
+          console.log(error);
+        }
+      }
+    }
+    fetchData();
+  }, [id]);
+
+  return <>{id}</>;
+}
--- frontend-estrangeum/src/services/apiObjects.js
+++ frontend-estrangeum/src/services/apiObjects.js
@@ -4,3 +4,8 @@ export async function getObjects() {
   const response = await api.get("/object/all");
   return response.data;
 }
+
+export async function getObject(id) {
+  const response = await api.get(`/object/${id}`);
+  return response.data;
+}
