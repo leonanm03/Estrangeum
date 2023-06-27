813af37b7950783e79f76a82672e166f8b470ed4
Leonan Martins <leonanm04@gmail.com>
2023-06-27 14:45:48 -0300

    feat: magnify on half done

--- frontend-estrangeum/src/components/index.js
+++ frontend-estrangeum/src/components/index.js
@@ -2,3 +2,6 @@ export * from "./NavBar";
 export * from "./Hello";
 export * from "./ObjectCard";
 export * from "./Objects";
+export * from "./ObjectDetails";
+export * from "./ZoomIcon";
+export * from "./ImageMagnifier";
