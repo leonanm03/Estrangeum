7744f7423501cad281253b0dde26289b7c759a31
Leonan Martins <leonanm04@gmail.com>
2023-07-07 10:00:00 -0300

    feat: subscription router

---
+++ backend-estrangeum/src/routers/subscription-router.ts
@@ -0,0 +1,38 @@
+import {
+  approveSubscription,
+  createSubscription,
+  findSubscriptionById,
+  getPendingSubscriptions,
+  rejectSubscription,
+} from "@/controllers";
+import {
+  authenticateAdminToken,
+  authenticateToken,
+  validateBody,
+  validateParams,
+} from "@/middlewares";
+import {
+  SubscriptionIdParamSchema,
+  SubscriptionPostSchema,
+} from "@/schemas/subscription-schemas";
+import { Router } from "express";
+
+const subscriptionRouter = Router();
+
+subscriptionRouter
+  .all("*", authenticateToken)
+  .post("/", validateBody(SubscriptionPostSchema), createSubscription)
+  .get("/:id", validateParams(SubscriptionIdParamSchema), findSubscriptionById)
+  .all("*", authenticateAdminToken)
+  .get("/pending", getPendingSubscriptions)
+  .post(
+    "/approve/:id",
+    validateParams(SubscriptionIdParamSchema),
+    approveSubscription
+  )
+  .put(
+    "/reject/:id",
+    validateParams(SubscriptionIdParamSchema),
+    rejectSubscription
+  );
+export default subscriptionRouter;
