809691d7c3e4b2d16d7a7351df6e15372e047887
Leonan Martins <leonanm04@gmail.com>
2023-07-10 19:33:53 -0300

    refactor: toastfy

--- frontend-estrangeum/src/pages/_app.jsx
+++ frontend-estrangeum/src/pages/_app.jsx
@@ -15,7 +15,18 @@ export default function App({ Component, pageProps }) {
         <NavBar />
         <Component {...pageProps} />
       </UserProvider>
-      <ToastContainer />
+      <ToastContainer
+        position="bottom-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
     </>
   );
 }
